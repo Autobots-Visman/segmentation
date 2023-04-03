0ad1ee1229a9c367701165d18b2b7986145da27a
Nicholas Gilpin <nicholascgilpin123@gmail.com>
2023-04-03 15:04:03 -0500

    add hungarian assignment

--- ros/object_detection/src/object_detection/segmentation.py
+++ ros/object_detection/src/object_detection/segmentation.py
@@ -2,11 +2,9 @@
 #     A script that performs semantic segmentation
 
 from ultralytics import YOLO
+from scipy.optimize import linear_sum_assignment
 import numpy as np
 import cv2
-
-# Configure
-debug = True
 
 # YOLOv8 segmentation models use the -seg suffix , i.e. yolov8n-seg.pt and are pretrained on COCO.
 model = YOLO('yolov8n-seg.pt')
@@ -55,48 +53,89 @@ def update_tracker(tracker, center_x, center_y):
     measurement = np.array([[np.float32(center_x)], [np.float32(center_y)]])
     tracker.correct(measurement)
     prediction = tracker.predict()
-    return prediction
 
 def get_center(xyxy):
+    """Convert a bounding box corner from the array boxes.xyxy to a center xy coordinate of the box. """
     bbox_center_x = (xyxy[2] + xyxy[0]) / 2
     bbox_center_y = (xyxy[3] + xyxy[1]) / 2
     return bbox_center_x, bbox_center_y
 
-def get_tracker(trackers, object_type, center_x, center_y):
-    """Return the coordinates of the nearest object of the same object type."""
-    if object_type in trackers:
-        min_distance = float('inf')
-        nearest_object = None
-        for oid, item in trackers[object_type].items():
-            distance = np.sqrt((center_x - item[1].statePost[0][0])**2 + (center_y - item[1].statePost[1][0])**2)
-            if distance < min_distance:
-                min_distance = distance
-                nearest_object = item
-        return nearest_object
-    else:
-        oid = str(object_type) + "1"
-        item = (oid, track())
-        trackers[object_type] = {oid : item}
-        return item
+def assign_trackers(trackers, boxes):
+    """Assign all detected bounding box centers to existing trackers with the least amount of error. """
+
+    # Calculate the center of all boxes of a known class
+    unused_boxes = set(range(len(boxes)))
+    all_assignments = set()
+    for object_class in trackers.keys():
+            known_centers = []
+            for i in range(len(boxes)):
+                if model.names[int(boxes[i].cls)] == object_class:
+                    unused_boxes.discard(i)
+                    bbox_center_x, bbox_center_y = get_center(boxes.xyxy[i])
+                    known_centers.append((bbox_center_x, bbox_center_y))
+
+            # Create a cost matrix with all trackers and centers of the object_class
+            cost_matrix = np.zeros((len(trackers[object_class]), len(known_centers)))
+            for i, (id, kalman_filter) in enumerate(trackers[object_class].items()):
+                for j, center in enumerate(known_centers):
+                    location = kalman_filter.statePost[:2].reshape(-1)
+                    cost_matrix[i][j] = np.linalg.norm(location - center)
+
+            # Use the hungarian algorithm to assign each bounding box center measurement to an existing kalman filter
+            row_ind, col_ind = linear_sum_assignment(cost_matrix)
+
+            # Convert the output to a list of tuples called assignments with the format (tracker_id, (center_x, center_y))
+            assignments = []
+            for i in range(len(row_ind)):
+                tracker_id = list(trackers[object_class].keys())[row_ind[i]]
+                center = known_centers[col_ind[i]]
+                assignments.append((tracker_id, center))
+                all_assignments.add(tracker_id)
+
+            # Update the trackers with the current center coordinate measurements
+            for tracker_id, center in assignments:
+                current_kf = trackers[object_class][tracker_id]
+                update_tracker(current_kf, center[0], center[1])
+
+    # If any tracker ids in trackers are unused, then update their kalman filter to lower the probability of their last known location. 
+    for object_class in trackers.keys():
+        for id in list(trackers[object_class].keys()):
+            if id not in all_assignments:
+                current_kf = trackers[object_class][id]
+                current_kf.errorCovPost *= 1.1
+
+    return unused_boxes
 
 try:
+    # Example: {"person": {"person1": kalman_filter, "person2": kalman_filter}, "chair": {"chair1": kalman_filter} }
     trackers = {}
     while True:
         frame   = get_frame(vid_stream)
         labels  = label(frame)
         boxes = labels.boxes
 
-        # Use a Kalman Filter to predict the location of each objectz
-        for i in range(len(boxes)):
-            object_type = model.names[int(boxes[i].cls)]
-            center_x, center_y = get_center(boxes.xyxy[i])
-            id, tracker = get_tracker(trackers, object_type, center_x, center_y)
-            location    = update_tracker(tracker, center_x, center_y)
-            print("Object:", id, "at:", location)
+        # Use a Kalman Filter to predict the location of each object
+        unused_boxes = assign_trackers(trackers, boxes)
 
-            # Display results
-            if debug:
-                # Display Predictions inside bounding boxes
+        # Create trackers for each boxes index in unused_boxes
+        for unused in list(unused_boxes):
+            object_class = model.names[int(boxes[unused].cls)]
+            if not object_class in trackers:
+                trackers[object_class] = {}
+            count = len(trackers[object_class])
+            id = object_class + str(count + 1)
+            trackers[object_class][id] = track()
+            center_x, center_y = get_center(boxes.xyxy[unused])
+            update_tracker(trackers[object_class][id], center_x, center_y)
+
+        # Use the estimated locations of all visible objects
+        for object_class in trackers.keys():
+            for id in trackers[object_class].keys():
+                kalman_filter = trackers[object_class][id]
+                location = np.round(np.array(kalman_filter.statePost[:2].reshape(-1)))
+
+                # Display results
+                print("Object:", id, "\tat:", (location))
                 cv2.rectangle(frame, (int(boxes.xyxy[i][0]), int(boxes.xyxy[i][1])), (int(boxes.xyxy[i][2]), int(boxes.xyxy[i][3])), (0, 255, 0), 2)
                 cv2.putText(frame, id, (int(boxes.xyxy[i][0]), int(boxes.xyxy[i][1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 cv2.drawMarker(frame, (int(location[0]), int(location[1])), (0, 0, 255), cv2.MARKER_CROSS, 20)
